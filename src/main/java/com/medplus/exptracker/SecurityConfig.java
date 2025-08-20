fed048fb5a03b3f8bf83315c38a057df9cbe7298
Vardhan Rangineni <ranginenivardhan@gmail.com>
2025-08-20 10:15:23 +0530

    Update SecurityConfig.java

--- src/main/java/com/medplus/exptracker/SecurityConfig.java
+++ src/main/java/com/medplus/exptracker/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.medplus.exptracker;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +30,20 @@ public class SecurityConfig {
 	
 		return http.build();
 	}
+	
+	  @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.addAllowedOrigin("http://localhost:3000");
+	        configuration.addAllowedMethod("*");
+	        configuration.addAllowedHeader("*");
+	        configuration.setAllowCredentials(true);
+	        
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
+
 	
 	
 	@Bean
