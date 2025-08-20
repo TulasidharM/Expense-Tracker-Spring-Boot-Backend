1313b61791d7458e47ac6ae6e06f5b7dbc5b5a71
Tulasidhar <110711019+TulasidharM@users.noreply.github.com>
2025-08-20 11:30:04 +0530

    change

--- src/main/java/com/medplus/exptracker/Controller/AuthController.java
+++ src/main/java/com/medplus/exptracker/Controller/AuthController.java
@@ -1,21 +1,30 @@
 package com.medplus.exptracker.Controller;
 
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthController {
-	@GetMapping("/login")
-	public String loginUser() {
+	@PostMapping("/login")
+	public ResponseEntity<Map<String, String>> loginUser() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		String roles = auth.getAuthorities().stream()
 						        .map(GrantedAuthority::getAuthority)
 						        .collect(Collectors.joining(", "));
-		return roles;
+		var res = new HashMap<String,String>();
+		res.put("role","ROLE_ADMIN");
+		return  ResponseEntity.ok(res);
 	}
 }
+
--- src/main/java/com/medplus/exptracker/SecurityConfig.java
+++ src/main/java/com/medplus/exptracker/SecurityConfig.java
@@ -9,9 +9,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +16,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http.csrf(csrf->csrf.disable());
+		http.cors(cors->cors.disable());
 		
 		http.authorizeHttpRequests(auth->{
 			auth.requestMatchers("/login").hasAnyRole("ADMIN","EMPLOYEE","MANAGER")
@@ -30,20 +28,6 @@ public class SecurityConfig {
 	
 		return http.build();
 	}
-	
-	  @Bean
-	    public CorsConfigurationSource corsConfigurationSource() {
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.addAllowedOrigin("http://localhost:3000");
-	        configuration.addAllowedMethod("*");
-	        configuration.addAllowedHeader("*");
-	        configuration.setAllowCredentials(true);
-	        
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-	        return source;
-	    }
-
 	
 	
 	@Bean
